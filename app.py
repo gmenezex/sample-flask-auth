6fb5ca1d8c3cf3a1524efa0d02e035f6571c5bb9
gmenezex <gabrielmenezes10121997@gmail.com>
2026-07-21 21:37:12 -0300

    Implementando niveis de usuarios e criptografia na senha

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -27,7 +28,7 @@ def login():
     #Login
     user = User.query.filter_by(username=username).first()
 
-    if user and user.password == password:
+    if user and bcrypt.checkpw(str.encode(password), user.password):
       login_user(user)
       print(current_user.is_authenticated)
       return jsonify({'message': 'Autenticação realizada com sucesso'})
@@ -42,14 +43,14 @@ def logout():
   return jsonify({'message': 'Logout realizado com sucesso'})
 
 @app.route('/user', methods=['POST'])
-@login_required
 def create_user():
   data = request.json
   username = data.get('username')
   password = data.get('password')
 
   if username and password:
-    user = User(username=username, password=password)
+    hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+    user = User(username=username, password=hashed_password, role='user')
     db.session.add(user)
     db.session.commit()
     return jsonify({"message": "Usuario cadastro com sucesso"})
@@ -72,6 +73,10 @@ def update_user(id_user):
   data = request.json
   user = User.query.get(id_user)
 
+
+  if id_user != current_user.id and current_user.role == "user":
+    return jsonify({"message": "Operação não permitida"}), 403
+
   if user and data.get("password"):
     user.password = data.get('password')
     db.session.commit()
@@ -85,6 +90,8 @@ def update_user(id_user):
 def delete_user(id_user):
   user = User.query.get(id_user)
 
+  if current_user.role != 'admin':
+    return jsonify({"message": "Operação não permitida"}), 403
   if id_user == current_user.id:
     return jsonify({"message": "Deleção não permitida"}), 403
 
